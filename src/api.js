cc6e135ca8c615adf8b8a68be14c2ec153d2a082
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-11-27 19:11:50 -0500

    Removing api module that isn't supposed to exist yet.

--- src/api.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
-
-const getItems = function () {
-  return Promise.resolve('A successful response!');
-};
-
-export default {
-  getItems
-};
